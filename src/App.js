6c82bd6700cf6635e6ec70a99e6edd038d232917
Siddhesh More <msiddhesh008@gmail.com>
2023-06-21 22:53:57 +0530

    second

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from './Pages/Home'
+import Home from "./Pages/Home";
 
 function App() {
   return (
     <div className="app">
-    <BrowserRouter>
-    <Routes>
-      <Route path="/react-portfolio" element={ <Home /> } />
-    </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/react-portfolio" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/Components/Intro.js
+++ src/Components/Intro.js
@@ -9,19 +9,19 @@ const Intro = () => {
 applications. I am proficient in React, Redux, and other related technologies. I am
 committed to delivering high-quality code and ensuring excellent user experiences. </p>
 <div className="row card-row justify-content-center m-lg-3 mt-5 mb-5 w-100">
-    <div className="col-lg-2 card col-5 b-1 m-2 ">
+    <div className="col-lg-2 card col-5 b-one m-2 ">
       <span>M</span>
       <p>Mongo</p>
     </div>
-    <div className="col-lg-2 card col-5 b-2 m-2 ">
+    <div className="col-lg-2 card col-5 b-two m-2 ">
       <span>E</span>
       <p>Express</p>
     </div>
-    <div className="col-lg-2 card col-5 b-3 m-2 ">
+    <div className="col-lg-2 card col-5 b-three m-2 ">
       <span>R</span>
       <p>React</p>
     </div>
-    <div className="col-lg-2 card col-5 b-4 m-2 ">
+    <div className="col-lg-2 card col-5 b-four m-2 ">
       <span>N</span>
       <p>Node</p>
     </div>
