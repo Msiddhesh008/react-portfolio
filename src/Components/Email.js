e8fcd40b3acc46407e41a7161bba517ae39cc593
Siddhesh More <msiddhesh008@gmail.com>
2023-06-26 00:10:30 +0530

    third commit

--- src/Components/Email.js
+++ src/Components/Email.js
@@ -1,7 +1,37 @@
 import React from "react";
 
 const Email = () => {
-  return <div id="email">Email</div>;
+  return (
+  <div 
+  className="d-lg-none"
+  id="email">
+    
+    <form className="form p-4"> 
+
+    <p className="lets-talk">Lets Talk</p>
+
+    <input 
+    placeholder="Name"
+    type="text" 
+    id="name" />
+
+    <input 
+    placeholder="Mobile"
+    type="tel" 
+    id="tel" />
+
+    <textarea 
+    placeholder="Message"
+    type="text" 
+    rows={5}
+    id="name" />
+
+    <button className="send-btn">Send</button>
+          
+    </form>
+
+  </div>
+  );
 };
 
 export default Email;
